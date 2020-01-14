71e84221a3fb57892a78ad72f326e07ced32488b
Jonathan Bungay <jabungay@mun.ca>
2020-01-13 21:19:06 -0330

    Fetches wheel diameter and odometer value from text file

--- Arduino/BikeComputer/BikeComputer.ino
+++ Arduino/BikeComputer/BikeComputer.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+// Pre-written includes
+#include <LiquidCrystal.h>
+
+// Custom includes
+#include "HelperFunctions.h"
+
+float wheelDiameter, odometer;
+File dataLogFile, parameterFile;
+
+// Initialize the LCD
+//const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+//LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() 
+{
+  // Initialize 16x2 character display
+//  lcd.begin(16,2);
+  // Initialize serial monitor
+  Serial.begin(9600);
+
+  // Retrieve wheelDiameter and odometer from parameters.txt
+  String parameterString = readFile("parameters.txt");
+  int newLine   = parameterString.indexOf((char) 10);
+  wheelDiameter = (parameterString.substring(0, newLine))).toInt();
+  odometer      = (parameterString.substring(newLine + 1))).toInt(); 
 
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 
 }
