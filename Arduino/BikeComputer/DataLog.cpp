018a4a0acd39f4073ce2785170b409e453fbc604
Jonathan Bungay <jabungay@mun.ca>
2020-01-15 21:48:20 -0330

    Created addDataPoint function

--- Arduino/BikeComputer/DataLog.cpp
+++ Arduino/BikeComputer/DataLog.cpp
@@ -1,1 +1,9 @@
+#include "DataLog.h"
 
+void addDataPoint(float speed, long odometer)
+{
+  // Retrieve the csv file
+  String dataFile = getFile("datalog.csv");
+
+
+}
--- Arduino/BikeComputer/DataLog.h
+++ Arduino/BikeComputer/DataLog.h
@@ -1,5 +1,9 @@
 #ifndef DATALOG_H
 #define DATALOG_H
 
+#include "HelperFunctions.h"
+
+// Function to add a data point to the pre-existing csv file
+void addDataPoint(float speed, long odometer);
 
 #endif
